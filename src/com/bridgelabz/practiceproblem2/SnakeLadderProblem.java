636d41a279d3f8b818dcfdaf3ed576e6d1ef4307
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 11:48:46 +0530

    [Rituparna]Added: Until 100 Loop Condition

--- src/com/bridgelabz/practiceproblem2/SnakeLadderProblem.java
+++ src/com/bridgelabz/practiceproblem2/SnakeLadderProblem.java
@@ -5,6 +5,7 @@ public class SnakeLadderProblem {
 	public static final int NO_PLAY = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
+    public static final int WINNING_POSITION = 100;
 
     public static void main(String[] args) {
 
@@ -12,22 +13,30 @@ public class SnakeLadderProblem {
                 "starts from 0 rolls the die to get a number between 1 to 6, finds\n" +
                 "a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.\n");
 
-        int playerPosition = 0;
+        int playerPosition = 0,totalDiceRolls = 0;
         System.out.println("Initial postion of the player: "+ playerPosition);
+        while(playerPosition<WINNING_POSITION)
+        {
+            totalDiceRolls++;
+            int diceRollNumber = (int)Math.floor(Math.random()*10)%6 +1;
+            //System.out.println("Outcome of Dice Roll: "+ diceRollNumber);
 
-        int diceRollNumber = (int)Math.floor(Math.random()*10)%6 +1;
-        System.out.println("Outcome of Dice Roll: "+ diceRollNumber);
+            int playerOption = (int)Math.floor(Math.random()*10)%3;
+            //System.out.println("Outcome of Player Option: "+ playerOption);
 
-        int playerOption = (int)Math.floor(Math.random()*10)%3;
-        System.out.println("Outcome of Player Option: "+ playerOption);
+            if (playerOption == LADDER)
+                playerPosition+=diceRollNumber;
+            else if(playerOption == SNAKE){
+                playerPosition-=diceRollNumber;
+                if(playerPosition<0)
+                    playerPosition=0;
+            }
 
-        if (playerOption == LADDER)
-            playerPosition+=diceRollNumber;
-        else if(playerOption == SNAKE)
-            playerPosition-=diceRollNumber;
-        else  if(playerOption ==NO_PLAY);
+            else  if(playerOption ==NO_PLAY);
+        }
 
         System.out.println("Player Position After Dice Roll & Player Option: "+ playerPosition);
+        System.out.println("Total Dice Rolls to reach Winning Position: "+totalDiceRolls);
     }
 
 
