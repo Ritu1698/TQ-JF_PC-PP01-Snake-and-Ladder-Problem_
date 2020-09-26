602901c51a04b77a6b499022706cc9b63b315b4a
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 11:46:32 +0530

    [Rituparna]Added: Snake, Ladder & NoPlay Option

--- src/com/bridgelabz/practiceproblem2/SnakeLadderProblem.java
+++ src/com/bridgelabz/practiceproblem2/SnakeLadderProblem.java
@@ -2,7 +2,11 @@ package com.bridgelabz.practiceproblem2;
 
 public class SnakeLadderProblem {
 	
-	public static void main(String[] args) {
+	public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+
+    public static void main(String[] args) {
 
         System.out.println("This problem simulates a Snake and Ladder Game. The Player" +
                 "starts from 0 rolls the die to get a number between 1 to 6, finds\n" +
@@ -13,7 +17,18 @@ public class SnakeLadderProblem {
 
         int diceRollNumber = (int)Math.floor(Math.random()*10)%6 +1;
         System.out.println("Outcome of Dice Roll: "+ diceRollNumber);
-        }
+
+        int playerOption = (int)Math.floor(Math.random()*10)%3;
+        System.out.println("Outcome of Player Option: "+ playerOption);
+
+        if (playerOption == LADDER)
+            playerPosition+=diceRollNumber;
+        else if(playerOption == SNAKE)
+            playerPosition-=diceRollNumber;
+        else  if(playerOption ==NO_PLAY);
+
+        System.out.println("Player Position After Dice Roll & Player Option: "+ playerPosition);
+    }
 
 
 }
