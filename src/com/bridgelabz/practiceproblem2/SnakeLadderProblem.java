e81c7a600da318661a0923f491f51ea1cf5880ad
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 11:41:07 +0530

    [Rituparna]Added: Welcome Msg & Initial Position

---
+++ src/com/bridgelabz/practiceproblem2/SnakeLadderProblem.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.practiceproblem2;
+
+public class SnakeLadderProblem {
+	
+	public static void main(String[] args) {
+
+        System.out.println("This problem simulates a Snake and Ladder Game. The Player" +
+                "starts from 0 rolls the die to get a number between 1 to 6, finds\n" +
+                "a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.\n");
+
+        int playerPosition = 0;
+        System.out.println("Initial postion of the player: "+ playerPosition);
+        }
+
+}
